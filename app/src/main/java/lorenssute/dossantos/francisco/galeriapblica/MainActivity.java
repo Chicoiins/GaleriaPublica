9dff653cb9ec2abbcf350e2732b7cdc737911f45
Chicoin <137420775+ChicoInputs@users.noreply.github.com>
2024-10-18 09:08:35 -0300

    Commit dia 18/10

--- app/src/main/java/lorenssute/dossantos/francisco/galeriapblica/MainActivity.java
+++ app/src/main/java/lorenssute/dossantos/francisco/galeriapblica/MainActivity.java
@@ -1,11 +1,16 @@
 package lorenssute.dossantos.francisco.galeriapblica;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -69,5 +74,58 @@ public class MainActivity extends AppCompatActivity {
         List<String> permissions = new ArrayList<>();
         permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         checkForPermissions(permissions);
+    }
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        for(String permission : permissions) {
+            if( !hasPermisson(permission)) {
+                permissionsNotGranted.add(permission);
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(permissionsNotGranted.size() > 0) {
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION); //PAREI NA PAG 26
+            }
+            else{
+                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+                int navigationOpSelected = vm.getNavigationOpSelected();
+                bottomNavigationView.setSelectedItemId(navigationOpSelected);
+            }
+        }
+    }
+
+    private boolean hasPermisson(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        final List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION) {
+            for (String permission : permissions) {
+                if (!hasPermisson(permission)) {
+                    permissionsRejected.add(permission);
+                }
+            }
+        }
+
+        if (permissionsRejected.size()) > 0 {
+            if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                    new AlertDialog.Builder(MainActivity.this).setMessage("Para usar essa app é preciso conceder essas permissões").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                        }
+                    }).create().show();
+                }
+            }
+        }
     }
 }
